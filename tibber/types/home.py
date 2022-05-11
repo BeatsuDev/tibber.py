e5238400660ff2d70639137c1f058e57e345cc60
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2022-05-11 04:25:36 +0200

    Added fetch_production and fetch_consumption methods

--- tibber/types/home.py
+++ tibber/types/home.py
@@ -4,13 +4,45 @@ from tibber.types.address import Address
 from tibber.types.metering_point_data import MeteringPointData
 from tibber.types.subscription import Subscription
 from tibber.types.home_features import HomeFeatures
-
+from tibber.types.home_consumption_connection import HomeConsumptionConnection
+from tibber.types.home_production_connection import HomeProductionConnection
+from tibber.networking import QueryBuilder
 
 class TibberHome:
     """A Tibber home with methods to get/fetch home information without the decorator functions to subscribe to live data."""
     def __init__(self, data: dict, tibber_client: "Client"):
         self.cache: dict = data
         self.tibber_client: "Client" = tibber_client
+
+    def fetch_consumption(self, 
+                          resolution: str, 
+                          first: int = None, 
+                          last: int = None, 
+                          before: str = None, 
+                          after: str = None, 
+                          filter_empty_nodes: bool = False) -> HomeConsumptionConnection:
+        consumption_query = QueryBuilder.consumption_query(resolution, first, last, before, after, filter_empty_nodes)
+        full_query = QueryBuilder.create_query("viewer", f"home(id: \"{self.id}\")", consumption_query)
+        unsanitized_data = self.tibber_client.execute_query(self.tibber_client.token, full_query)
+
+        # The format should be correct, since we requested it this way and the request
+        # was successful, so we don't need to worry about key errors. 
+        data = unsanitized_data["viewer"]["home"]["consumption"]
+        return HomeConsumptionConnection(resolution, data, self.tibber_client)
+
+    def fetch_production(self,
+                         resolution: str = None, 
+                         first: int = None, 
+                         last: int = None, 
+                         before: str = None, 
+                         after: str = None, 
+                         filter_empty_nodes: bool = False) -> HomeProductionConnection:
+        production_query = QueryBuilder.production_query(resolution, first, last, before, after, filter_empty_nodes)
+        full_query = QueryBuilder.create_query("viewer", f"home(id: \"{self.id}\")", production_query)
+        unsanitized_data = self.tibber_client.execute_query(self.tibber_client.token, full_query)
+        
+        data = unsanitized_data["viewer"]["home"]["production"]
+        return HomeProductionConnection(resolution, data, self.tibber_client)
 
     @property
     def id(self) -> str:
