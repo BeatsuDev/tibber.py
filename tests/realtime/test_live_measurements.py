acf3ae7a67a161149a9f0f16ab4ecd90725256bc
Eric B <ericbieszczad@gmail.com>
2022-06-13 09:10:42 +0200

    Added live measurement test

---
+++ tests/realtime/test_live_measurements.py
@@ -0,0 +1,30 @@
+"""Tests for reading live data from a tibber.TibberHome"""
+import pytest
+from datetime import datetime
+from datetime import timedelta
+
+import tibber
+
+
+@pytest.fixture
+def home():
+    client = tibber.Client(tibber.DEMO_TOKEN)
+    try:
+        return client.homes[0]
+    except IndexError:
+        raise ValueError("The instanciated demo client does not have any homes. Cannot perform home tests.")
+
+
+def test_realtime_live_measurements(home):  
+    import time
+    start_time = time.perf_counter()
+
+    @home.event("live_measurement")
+    def callback(data):
+        timestamp = datetime.strptime(data.timestamp, "%Y-%m-%dT%H:%M:%S.%f%z")
+        # This *might* cause errors on really slow networks. 
+        assert (datetime.now().astimezone() - timestamp) < timedelta(seconds=5)
+        raise RuntimeError("Exit the loop!")
+
+    with pytest.raises(RuntimeError):
+        home.start_livefeed()
