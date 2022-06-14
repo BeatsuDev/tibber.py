08048628ca6f74b2e684e2dbd9342f32071b5303
Eric B <ericbieszczad@gmail.com>
2022-06-14 22:34:29 +0200

    Increment version

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 DEMO_TOKEN = "476c477d8a039529478ebd690d35ddd80e3308ffc49b59c65b142321aee963a4"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
 SUBSCRIPTION_ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions"
