9f656f9a689dec9c2df4a702a1761329a2789328
Eric B <ericbieszczad@gmail.com>
2022-06-14 22:38:55 +0200

    Potential fix for #8 ...

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -1,12 +1,9 @@
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 DEMO_TOKEN = "476c477d8a039529478ebd690d35ddd80e3308ffc49b59c65b142321aee963a4"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
 SUBSCRIPTION_ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions"
 
 # Import modules after defining constants to avoid circular import error.
-import tibber.networking
-import tibber.exceptions
-import tibber.types
 from .client import Client
 
 from .types.home import TibberHome
