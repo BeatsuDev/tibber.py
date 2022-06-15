ac05104c9f396a4add8d95820a6cf7facdaadfa8
Eric B <ericbieszczad@gmail.com>
2022-06-15 15:43:04 +0200

    Increment version

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.0-1"  # Build version where issue #8 is fixed 
+__version__ = "0.1.0"  # Build version where issue #8 is fixed 
 DEMO_TOKEN = "476c477d8a039529478ebd690d35ddd80e3308ffc49b59c65b142321aee963a4"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
 SUBSCRIPTION_ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions"
