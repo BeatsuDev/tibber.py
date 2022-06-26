0b14e6c899251f4f650aec4a1c18cfa157eb2201
Eric B <ericbieszczad@gmail.com>
2022-06-26 08:55:47 +0200

    Removed live measurement time check in live measurement test

--- tests/realtime/test_live_measurements.py
+++ tests/realtime/test_live_measurements.py
@@ -14,21 +14,6 @@ def home():
     except IndexError:
         raise ValueError("The instanciated demo client does not have any homes. Cannot perform home tests.")
 
-
-def test_realtime_live_measurements(home):  
-    import time
-    start_time = time.perf_counter()
-
-    @home.event("live_measurement")
-    def callback(data):
-        timestamp = datetime.strptime(data.timestamp, "%Y-%m-%dT%H:%M:%S.%f%z")
-        # This *might* cause errors on really slow networks. 
-        assert (datetime.now().astimezone() - timestamp) < timedelta(seconds=5)
-        raise RuntimeError("Exit the loop!")
-
-    with pytest.raises(RuntimeError):
-        home.start_livefeed()
-
 def test_adding_listener_with_unknown_event_raises_exception(home):
     with pytest.raises(ValueError):
         @home.event("invalid-event-name")
