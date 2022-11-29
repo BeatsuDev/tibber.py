2e409097a5acb46a98f8a0f324c578dd184cc171
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2022-11-29 18:30:11 +0100

    Removed subscription endpoint

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -1,7 +1,6 @@
 __version__ = "0.2.0"
 DEMO_TOKEN = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
-SUBSCRIPTION_ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions"
 
 # Import modules after defining constants to avoid circular import error.
 from .account import Account
