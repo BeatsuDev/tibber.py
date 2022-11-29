467d8a8337e7c6f47e740eb298f4d5953ca9ea0b
Eric Bieszczad-Stie <ericbi@stud.ntnu.no>
2022-11-29 05:05:33 +0100

    Fixed consumption tests

--- tests/fetched/test_consumption.py
+++ tests/fetched/test_consumption.py
@@ -16,7 +16,7 @@ def home():
         raise ValueError("The instanciated demo account does not have any homes. Cannot perform home tests.")
 
 def test_consumption_page_info(home):
-    home_consumption_connection = home.fetch_consumption("HOURLY", first=5, after="MjAxOC0xMS0wM1QyMDowMDowMC4wMDArMDE6MDA=")
+    home_consumption_connection = home.fetch_consumption("HOURLY", first=5, after="MjAyMi0xMS0yOFQxODowMDowMC4wMDArMDE6MDA=")
     page_info = home_consumption_connection.page_info
 
     assert page_info.end_cursor == home_consumption_connection.edges[-1].cursor
@@ -26,18 +26,20 @@ def test_consumption_page_info(home):
 
     assert page_info.count == 5
     assert page_info.currency == "SEK"
-    assert page_info.total_cost == 6.151398125
-    assert page_info.total_consumption == 9.45
+    assert page_info.total_cost == 39.5775870375
+    assert page_info.total_consumption == 15.132
     assert page_info.filtered == 0
 
 def test_consumption_nodes(home):
-    home_consumption_connection = home.fetch_consumption("HOURLY", first=5)
+    home_consumption_connection = home.fetch_consumption("HOURLY", first=3, after="MjAyMi0xMS0yOFQxODowMDowMC4wMDArMDE6MDA=")
     history = home_consumption_connection.nodes
 
-    assert history[0].from_time == "2018-11-02T00:00:00.000+01:00"
-    assert history[0].to_time == "2018-11-02T01:00:00.000+01:00"
+    assert history[0].from_time == "2022-11-28T19:00:00.000+01:00"
+    assert history[0].to_time == "2022-11-28T20:00:00.000+01:00"
 
-    assert [node.unit_price for node in history] == [0.6300375, 0.614075, 0.61175, 0.6085375, 0.6163875]
-    assert [node.unit_price_vat for node in history] == [0.1260075, 0.122815, 0.12235, 0.1217075, 0.1232775]
+    assert [node.cost for node in history] == [7.95558735, 8.85535695, 7.205103525]
+    assert [node.unit_price for node in history] == [3.09195, 2.854725, 2.689475]
+    assert [node.unit_price_vat for node in history] == [0.61839, 0.570945, 0.537895]
+    assert [node.consumption for node in history] == [2.573, 3.102, 2.679]
     assert all(node.consumption_unit == "kWh" for node in history)
 
