9fde717905f36bf21cf4eb0ab7e2136b6305caec
Eric B <ericbieszczad@gmail.com>
2022-12-01 13:12:06 +0100

    Fix OSError on exit

--- tibber/__init__.py
+++ tibber/__init__.py
@@ -2,6 +2,12 @@ __version__ = "0.2.0"
 DEMO_TOKEN = "5K4MVS-OjfWhK_4yrjOlFe1F6kJXPVf7eQYggo8ebAE"
 API_ENDPOINT = "https://api.tibber.com/v1-beta/gql"
 
+# The event loop type causes problems on windows systems when exiting.
+import os
+import asyncio
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 # Import modules after defining constants to avoid circular import error.
 from .account import Account
 
