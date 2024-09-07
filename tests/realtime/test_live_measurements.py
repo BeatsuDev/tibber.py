5c02a365485ee2fb1c5729b4c0d90d6b37c88ae1
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-09-07 17:52:37 +0200

    test: remove a lot of unnecessary checks

--- tests/realtime/test_live_measurements.py
+++ tests/realtime/test_live_measurements.py
@@ -37,31 +37,6 @@ def test_retrieving_live_measurements():
         timestamp = timestamp.replace(tzinfo=None)
         now = datetime.now().replace(tzinfo=None)
         assert timestamp > now - timedelta(seconds=30)
-        assert data.power > 0
-        assert data.last_meter_consumption > 0
-        assert data.accumulated_consumption > 0
-        assert isinstance(data.accumulated_production, (int, float))
-        assert data.accumulated_consumption_last_hour > 0
-        assert isinstance(data.accumulated_production_last_hour, (int, float))
-        assert data.accumulated_cost > 0
-        assert isinstance(data.accumulated_reward, (int, float))
-        assert data.currency == "SEK"
-        assert data.min_power >= 0
-        assert data.max_power > 0
-        assert data.average_power > 0
-        assert isinstance(data.power_production, (int, float))
-        assert isinstance(data.power_reactive, (int, float))
-        assert data.power_production_reactive > 0
-        assert isinstance(data.min_power_production, (int, float))
-        assert isinstance(data.max_power_production, (int, float))
-        assert data.last_meter_production > 0
-        assert data.power_factor > 0
-        assert data.voltage_phase_1 > 0
-        assert data.voltage_phase_2 > 0
-        assert data.voltage_phase_3 > 0
-        assert data.currentL1 > 0
-        assert data.currentL2 > 0
-        assert data.currentL3 > 0
 
     # Return immediately after the first callback
     home.start_live_feed(f"tibber.py-tests/{__version__}", exit_condition = lambda data: True)
