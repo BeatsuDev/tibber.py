c90297bdc5dfc987fb02eb06dde52996cea68111
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-09-07 17:54:12 +0200

    test: add timeouts for realtime tests

--- tests/realtime/test_live_measurements.py
+++ tests/realtime/test_live_measurements.py
@@ -14,6 +14,7 @@ def test_adding_listener_with_unknown_event_raises_exception(home):
         async def callback(data):
             print(data)
 
+@pytest.mark.timeout(60)
 def test_starting_live_feed_with_no_listeners_shows_warning(caplog):
     account = tibber.Account(tibber.DEMO_TOKEN)
     home = account.homes[0]
@@ -22,12 +23,14 @@ def test_starting_live_feed_with_no_listeners_shows_warning(caplog):
     home.start_live_feed(f"tibber.py-tests/{__version__}", exit_condition = lambda data: True)
     assert "The event that was broadcasted has no listeners / callbacks! Nothing was run." in caplog.text
 
+@pytest.mark.timeout(60)
 def test_retrieving_live_measurements():
     account = tibber.Account(tibber.DEMO_TOKEN)
     home = account.homes[0]
 
     global callback_was_run
     callback_was_run = False
+
     @home.event("live_measurement")
     async def callback(data):
         global callback_was_run
