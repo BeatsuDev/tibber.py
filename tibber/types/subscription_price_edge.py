0955d1cd20cd9a1da9a33d379d29483dd6dc586a
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-01-06 14:46:10 +0100

    feat: add classes necessary for range method of PriceInfo

---
+++ tibber/types/subscription_price_edge.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+"""A class representing the SubscriptionPriceEdge type from the GraphQL Tibber API."""
+from typing import TYPE_CHECKING
+
+from tibber.types.price import Price
+
+
+class SubscriptionPriceEdge:
+    def __init__(self, data: dict, tibber_client: "Account"):
+        self.cache: dict = data or {}
+        self.tibber_client: "Account" = tibber_client
+
+    @property
+    def cursor(self) -> str:
+        return self.cache.get("cursor")
+
+    @property
+    def node(self) -> Price:
+        return Price(self.cache.get("node"), self.tibber_client)
