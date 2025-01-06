2f809bb37d101227be2275de2bc4e8ad08395b0e
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-01-06 21:40:40 +0100

    chore: use specific timezone in range test

--- tests/fetched/test_range.py
+++ tests/fetched/test_range.py
@@ -1,6 +1,7 @@
 """Tests fetching historical price data."""
 import base64
 from datetime import datetime
+from datetime import timezone
 from datetime import timedelta
 
 import pytest
@@ -9,7 +10,7 @@ import tibber
 
 
 def test_fetching_hourly_prices(home):
-    date = datetime(2025, 1, 1, 0, 0, 0)
+    date = datetime(2025, 1, 1, 0, 0, 0, tzinfo=timezone(timedelta(hours=1)))
     encoded = base64.b64encode(date.astimezone().isoformat().encode("utf-8")).decode("utf-8")
 
     data = home.current_subscription.price_info.fetch_range("HOURLY", first="3", after=encoded)
